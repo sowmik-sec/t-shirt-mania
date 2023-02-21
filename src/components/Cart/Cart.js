84d47d289aae7e3d7b17be3b10ae3aadfcf7465c
sowmik-sec <sowmik.sec@gmail.com>
2023-02-21 11:56:33 +0600

    conditional rendering

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,19 @@
 import React from "react";
 
 const Cart = ({ cart, handleRemoveItem }) => {
+  let message;
+  if (cart.length === 0) {
+    message = <p>Please buy at least one item</p>;
+  } else if (cart.length === 1) {
+    message = (
+      <div>
+        <h3>Please, buy 1 for me</h3>
+        <p>Buy another for a poor guy</p>
+      </div>
+    );
+  } else {
+    message = <p>Thanks for buying</p>;
+  }
   return (
     <div>
       <h3>Order Summary</h3>
@@ -11,8 +24,13 @@ const Cart = ({ cart, handleRemoveItem }) => {
           <button onClick={() => handleRemoveItem(tshirt)}>X</button>
         </p>
       ))}
+      {message}
     </div>
   );
 };
 
 export default Cart;
+
+/*
+1. use element and if-else
+*/
