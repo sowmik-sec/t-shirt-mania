c0b2d2cc93bb41cd43f4419bff9bb11b2af9bb92
sowmik-sec <sowmik.sec@gmail.com>
2023-02-21 10:40:22 +0600

    added some components and creating router

---
+++ src/__steps.js
@@ -0,0 +1,8 @@
+/*
+1. npm i react-router-dom
+2. create a router using createBrowserRouter
+3. add RouterProvider and set router prop
+4. Create a lot of components
+5. Create main layout with some common part and one changing part based on the route
+6.
+*/
